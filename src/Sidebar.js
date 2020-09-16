34e8968e68ba64411c681c0d801be782d44f51d4
krisszcode <nagy.ace.krisztian@gmail.com>
2020-09-16 18:26:54 +0200

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://i.pinimg.com/originals/a4/4a/f3/a44af3bb5f074e3cdb4be8a56232c996.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9RWiHPnFOyRzrXWfjo87Tj8dD2313Las",
+    authDomain: "whatsapp-krissz.firebaseapp.com",
+    databaseURL: "https://whatsapp-krissz.firebaseio.com",
+    projectId: "whatsapp-krissz",
+    storageBucket: "whatsapp-krissz.appspot.com",
+    messagingSenderId: "561996138195",
+    appId: "1:561996138195:web:8afaf9eb52db68e1953626"
+  };
